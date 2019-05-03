62c031d1bd0e8f21d2bb7c6867a529efa53ecd43
excellencetechnologies <excellencetechnologies>
2019-05-03 16:23:56 +0530

    update average report module for getting average category wise

--- app/IncomeExpense.php
+++ app/IncomeExpense.php
@@ -85,34 +85,30 @@ class IncomeExpense extends Model
         $userid = Auth::id();
         $user = User::find($userid);
 
-        $expense = $data['expense'];
-        $categories = json_encode($data['categories']);
-        
         $amount = [];
-        foreach( $data['categories'] as $categories_iters ) {
-            foreach( $categories_iters as $key => $category ){
-                foreach( $category as $sub_category ){
-                    $amount[] = $sub_category['amount'];
-                }
+        foreach( $data['categories'] as $categories ) {
+            foreach( $categories as $category ){
+                $amount[] = $category['amount'];
             }
         }
         
-        $total_amount = array_sum($amount);        
-        if( $total_amount != $expense ){
+        $total_amount = array_sum($amount);
+        if( $total_amount != $data['expense'] ){
             throw new Exception('Expense is not matching the total amout spending on different categories.');
         }
         
         $this->user_id = $userid;
-        $this->expense = $expense;
-        $this->balance = $user->balance - $expense;
-        $this->categories = $categories;
+        $this->expense = $data['expense'];
+        $this->balance = $user->balance - $data['expense'];
+        $this->categories = json_encode($data['categories']);
         $this->save();
 
-        $user->balance = $user->balance - $expense;
+        $user->balance = $user->balance - $data['expense'];
         $user->save();
 
         $user = array_merge($user->toArray(), $this->toArray());
-        
+        $user['categories'] = json_decode($user['categories'], true);
+
         return $user;
     }
 
@@ -138,10 +134,10 @@ class IncomeExpense extends Model
 
         if( $year ){
             if( $month ){
-                $report = $this::find($userid)->whereMonth('created_at', $month)->whereYear('created_at', $year)->get();
+                $report = $this::find($userid)->whereMonth('created_at', $month)->whereYear('created_at', $year)->get()->toArray();
 
             } else {
-                $report = $this::find($userid)->whereYear('created_at', $year)->get();
+                $report = $this::find($userid)->whereYear('created_at', $year)->get()->toArray();
             }
 
         } else {
@@ -150,8 +146,12 @@ class IncomeExpense extends Model
                 if( $validator->fails() ){ return response()->json(['error' => $validator->errors()]); }
 
             } else {
-                $report = $this::find($userid)->get();
+                $report = $this::find($userid)->get()->toArray();
             }
+        }
+
+        foreach($report as $key => $report_value) {
+            $report[$key]['categories'] = json_decode($report_value['categories'], true);
         }
 
         return $report;
@@ -177,27 +177,33 @@ class IncomeExpense extends Model
         $month = isset($data['month']) ? $data['month'] : null;
         $year = isset($data['year']) ? $data['year'] : null;
         $expense = $income = $categories = [];
-        $allCategories = array_map(function($item){
-            return $item['name'];
-        }, Categories::all()->toArray());
-
-
+        
         if( $year ){
             if( $month ){
                 $expense = $this::find($userid)->whereMonth('created_at', $month)->whereYear('created_at', $year)->whereNotNull('expense')->avg('expense');
                 $income = $this::find($userid)->whereMonth('created_at', $month)->whereYear('created_at', $year)->whereNotNull('income')->avg('income');
-                foreach( $allCategories as $category ){
-                    $category_average = $this::find($userid)->whereMonth('created_at', $month)->whereYear('created_at', $year)->whereNotNull('expense')->whereNotNull($category)->avg($category);
-                    $categories[$category] = isset($category_average) ? $category_average : null;
+                $user_expense_categories = $this::find($userid)->select('categories')->whereMonth('created_at', $month)->whereYear('created_at', $year)->whereNotNull('expense')->get()->toArray();
+                
+                $user_expense_categories_array = [];
+                foreach( $user_expense_categories as $user_expense_category ){
+                    $user_expense_categories_array[] = json_decode($user_expense_category['categories'], true);
                 }
+                
+                $categories = $this->group_categories( $user_expense_categories_array );
+                $average_categories_report = $this->average_report( $categories );
                 
             } else {
                 $expense = $this::find($userid)->whereYear('created_at', $year)->whereNotNull('expense')->avg('expense');
                 $income = $this::find($userid)->whereYear('created_at', $year)->whereNotNull('income')->avg('income');
-                foreach( $allCategories as $category ){
-                    $category_average = $this::find($userid)->whereYear('created_at', $year)->whereNotNull('expense')->whereNotNull($category)->avg($category);
-                    $categories[$category] = isset($category_average) ? $category_average : null;
+                $user_expense_categories = $this::find($userid)->select('categories')->whereYear('created_at', $year)->whereNotNull('expense')->get()->toArray();
+
+                $user_expense_categories_array = [];
+                foreach( $user_expense_categories as $user_expense_category ){
+                    $user_expense_categories_array[] = json_decode($user_expense_category['categories'], true);
                 }
+
+                $categories = $this->group_categories( $user_expense_categories_array );
+                $average_categories_report = $this->average_report( $categories );
             }
 
         } else {
@@ -208,21 +214,75 @@ class IncomeExpense extends Model
             } else {
                 $expense = $this::find($userid)->whereNotNull('expense')->avg('expense');
                 $income = $this::find($userid)->whereNotNull('income')->avg('income');
-                foreach( $allCategories as $category ){
-                    $category_average = $this::find($userid)->whereNotNull('expense')->whereNotNull($category)->avg($category);
-                    $categories[$category] = isset($category_average) ? $category_average : null;
+                $user_expense_categories = $this::find($userid)->select('categories')->whereNotNull('expense')->get()->toArray();
+
+                $user_expense_categories_array = [];
+                foreach( $user_expense_categories as $user_expense_category ){
+                    $user_expense_categories_array[] = json_decode($user_expense_category['categories'], true);
                 }
+
+                $categories = $this->group_categories( $user_expense_categories_array );
+                $average_categories_report = $this->average_report( $categories );
             }
         }
 
         $average_report = [
             'month' => $month,
             'year' => $year,
+            'user_id' => $userid,
             'average_income' => $income,
             'average_expesne' => $expense,
-            'average_expense_categories' => $categories
+            'average_expense_categories' => $average_categories_report
         ];         
         
+        return $average_report;
+    }
+
+    public function group_categories( $categories )
+    {
+        $categories_count_array = [];
+        foreach( $categories as $categories_iters ){
+            foreach( $categories_iters as $category => $sub_categories ) {
+                if( !array_key_exists( $category, $categories_count_array ) ){
+                    $categories_count_array[$category]['count'] = 1;
+                } else {
+                    $categories_count_array[$category]['count'] += 1;
+                }
+                foreach( $sub_categories as $sub_category ){                            
+                    if( !array_key_exists( 'sub_categories', $categories_count_array[$category] ) ){                                
+                        $categories_count_array[$category]['amount'] = $sub_category['amount'];
+                        $categories_count_array[$category]['sub_categories'][$sub_category['sub_category_name']]['count'] = 1;
+                        $categories_count_array[$category]['sub_categories'][$sub_category['sub_category_name']]['amount'] = $sub_category['amount'];
+
+                    } else {
+                        if( array_key_exists( $sub_category['sub_category_name'], $categories_count_array[$category]['sub_categories'] ) ){
+                            $categories_count_array[$category]['amount'] += $sub_category['amount'];
+                            $categories_count_array[$category]['sub_categories'][$sub_category['sub_category_name']]['count'] += 1;
+                            $categories_count_array[$category]['sub_categories'][$sub_category['sub_category_name']]['amount'] += $sub_category['amount'];
+
+                        } else {
+                            $categories_count_array[$category]['amount'] += $sub_category['amount'];
+                            $categories_count_array[$category]['sub_categories'][$sub_category['sub_category_name']]['count'] = 1;
+                            $categories_count_array[$category]['sub_categories'][$sub_category['sub_category_name']]['amount'] = $sub_category['amount'];
+                        }                               
+                    }
+                }
+            }
+        }
+
+        return $categories_count_array;
+    }
+
+    public function average_report( $categories )
+    {
+        $average_report = [];
+        foreach( $categories as $category => $category_iters ){
+            $average_report[$category]['average'] = $category_iters['amount'] / $category_iters['count'];
+            foreach( $category_iters['sub_categories'] as $sub_category => $sub_category_value ){
+                $average_report[$category]['sub_categories'][$sub_category] = $sub_category_value['amount'] / $sub_category_value['count'];
+            }
+        }
+
         return $average_report;
     }
 }
